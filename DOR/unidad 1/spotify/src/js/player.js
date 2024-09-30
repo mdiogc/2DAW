e85f9f92cd65b7c167bdcf3fd1899a6436e0c6d4
alu <mdiogcasko@gmail.com>
2024-09-30 20:25:23 +0100

    maldito playerjs

---
+++ DOR/unidad 1/spotify/src/js/player.js
@@ -0,0 +1,12 @@
+import Song, { play_song } from "./song.js";
+
+export default class Player {
+  constructor(map) {
+    Object.entries(map);
+    let aux = 1;
+    for (var [key, value] of Object.entries(map)) {
+      // Crea la instancia para Song
+      // Invoca la funcion para reproducir la cancion
+    }
+  }
+}
