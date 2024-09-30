d2309908267e9176d31b24758aea39d69cc1b565
alu <mdiogcasko@gmail.com>
2024-09-30 20:09:22 +0100

    maldito asterisco

--- DOR/unidad 1/spotify/src/js/index.js
+++ DOR/unidad 1/spotify/src/js/index.js
@@ -1,4 +1,4 @@
-import songs from "../assets/songs/1.mp3";
+import songs from "../assets/songs/*.mp3";
 
 Object.keys(songs);
 
@@ -8,7 +8,7 @@ let aux = 1;
 
 for (var key of Object.keys(songs)) {
   //asociar map [`.item-${aux}`] con la cancion songs[key]
-  map[".item" + aux++] = key;
+  map[".item" + aux++] = songs[key];
 }
 console.log(map);
 // crear un objeto player y pasar el objeto map
