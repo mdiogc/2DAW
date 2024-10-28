a3f4a67e56d643ad064464f7801eeea1390e5dfa
mdiogc <mdiogcasko@gmail.com>
2024-10-28 16:36:14 +0000

    no soporto a nadie

---
+++ DEW/UT3/pesadilla en typescript/src/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Ejercicio D1S1.1.1
+console.log("Hola");
+// Ejercicio D1S1.1.2
+// Ejercicio D1S2.1.1
+// Ejercicio D1S2.1.2
+// Ejercicio D1S2.2.1
+// Ejercicio D1S2.2.2
---
+++ DEW/UT3/pesadilla en typescript/src/script.ts
@@ -0,0 +1,14 @@
+// Ejercicio D1S1.1.1
+console.log("Hola")
+// Ejercicio D1S1.1.2
+
+// Ejercicio D1S2.1.1
+
+// Ejercicio D1S2.1.2
+
+// Ejercicio D1S2.2.1
+
+// Ejercicio D1S2.2.2
+
+        
+        
